264591fcbe4b1b6b9fb26497fbbb72518d43cc85
zkj <438101845@qq.com>
2020-07-29 18:05:44 +0800

    [zkj]-feat: finish findCommonWordWithSpace.function

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,35 @@
+//import sun.plugin.javascript.navig.Array;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GrammarExercise {
     public static void main(String[] args) {
         //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+        String firstWordList = "apple,juice,mother,people,beautiful,apple,dog";
+        String secondWordList = "cat,baby,smile,good,apple,beautiful,Dog,nice";
 
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
         //按要求输出到命令行
-
+        System.out.println(result);
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        String[] firstArr = firstWordList.split(",");
+        String[] secondArr = secondWordList.split(",");
+        return Arrays.stream(firstArr)
+                .distinct()
+                .map(String::toUpperCase)
+                .filter(s -> Arrays.stream(secondArr)
+                            .distinct()
+                            .map(String::toUpperCase)
+                            .collect(Collectors.toList())
+                            .contains(s)
+                )
+                .map(s -> String.join(" ", s.split("")))
+                .collect(Collectors.toList());
     }
 }
